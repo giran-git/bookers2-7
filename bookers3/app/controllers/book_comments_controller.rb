4c30758d59c9b08aa3bb108396a5df98e95598d7
giran-git <giran904587034+git@gmail.com>
2021-06-06 08:51:08 +0000

    コメント機能非同期

--- bookers3/app/controllers/book_comments_controller.rb
+++ bookers3/app/controllers/book_comments_controller.rb
@@ -6,10 +6,11 @@ before_action :authenticate_user!
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
+		unless @book_comment.save
+			render 'error'
+  # 		redirect_to book_path(@book.id)
+		# else
+		#   render 'books/show'
 		end
 	end
 
