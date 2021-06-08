c10881136476303006a85b07f785515fa14cdcf1
giran-git <giran904587034+git@gmail.com>
2021-06-08 15:00:49 +0000

    応用課題

---
+++ bookers3/app/models/book.rb
@@ -0,0 +1,24 @@
+class Book < ApplicationRecord
+	belongs_to :user
+
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
+	def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+end
